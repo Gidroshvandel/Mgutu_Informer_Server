89d3fad4a8639a9d7f7b5934544f64ab15e0a02c
Gidro <Gidroshvandel@gmail.com>
2016-02-10 21:43:09 +0300

    Add Velocity

--- src/main/java/controller/api/SiteRoutes.java
+++ src/main/java/controller/api/SiteRoutes.java
@@ -3,6 +3,10 @@ package controller.api;
 import controller.BaseController;
 import controller.logic.UserController;
 import model.Users;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
 
 import static spark.Spark.get;
 import static spark.Spark.post;
@@ -10,6 +14,13 @@ import static spark.Spark.post;
 public class SiteRoutes extends BaseController{
     @Override
     public void routes() {
+        get("/advice_form", (request, response) -> {
+            System.out.println("Выполняется /advice_form");
+            return new ModelAndView(new HashMap(), "/public/templates/form.vtl");
+        }, new VelocityTemplateEngine());
+        get("/index",(request, response) -> {
+            return new ModelAndView(new HashMap(),"/public/index.html");
+        }, new VelocityTemplateEngine());
         post("/users/registration",(request, response) -> {
             Users registration = new Users(request.queryParams("name"),request.queryParams("groups"),request.queryParams("login"),request.queryParams("password"), Boolean.parseBoolean(request.queryParams("student")));
             UserController userController = new UserController(registration);
@@ -19,7 +30,6 @@ public class SiteRoutes extends BaseController{
 
         post("/users/login", (request, response) -> {
 
-//            Users login = new Users(request.queryParams("name"),request.queryParams("groups"),request.queryParams("login"),request.queryParams("password"), Boolean.parseBoolean(request.queryParams("student")));
             Users login = new Users(request.queryParams("login"), request.queryParams("password"));
 
             UserController userController = new UserController(login);
