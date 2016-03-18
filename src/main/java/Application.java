affa538fd7349d08ec3e61b39d779ce2b2db247a
Gidro <Gidroshvandel@gmail.com>
2016-03-18 21:35:57 +0300

    Add for Heroku

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,7 +8,10 @@ public class Application {
     public static void main(String[] args){
 
         staticFileLocation("/public");
-        port(5555);
+//        port(5555);
+        int port;
+        port = Integer.parseInt(args[0]);
+        port(port);
         new MobileRoutes();
         new SiteRoutes();
     }
